36647a77d16364bf8f989ba0e56717d0c96752b2
Ankit Sharma <>
2022-12-10 18:04:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var readlineSync = require("readline-sync");
+
+var score = 0;
+
+var userName = readlineSync.question("What is your name? ");
+console.log("----------------------------------------------------------")
+console.log("Welcome " + userName + "! to ---- DO YOU KNOW ANKIT ? ----");
+console.log("----------------------------------------------------------")
+
+// PLAY FUNCTION
+
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+  if (userAnswer === answer) {
+    console.log("You are right!");
+    score = score + 1;
+
+  } else {
+    console.log("You are wrong!");
+  }
+  console.log("Current Score : " + score);
+  console.log("---------------------")
+}
+
+// ARRAY OF QUESTIONS
+
+var questions = [
+  {
+    question: "Where do i live? ",
+    answer: "Bangalore"
+  },
+  {
+    question: "What is my favourite food? ",
+    answer: "Samosa"
+  },
+  {
+    question: "What is my age? ",
+    answer: "22"
+  }
+]
+
+// for loop
+
+for (var i=0; i<questions.length; i++) {
+  var currentQuestion = questions[i];
+  play(currentQuestion.question, currentQuestion.answer);
+}
+
+console.log("YAY ! You Scored : " + score);
